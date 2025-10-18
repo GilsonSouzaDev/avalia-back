035f110839ce092476f58c36fa3cb3acecf1a4af
GilsonSouzaDev <gilsonsouza.dev@gmail.com>
2025-10-18 02:26:02 -0300

    feat: construção e configuração inicial do projeto

---
+++ src/test/java/com/fatec/avalia/AvaliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.avalia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
