336174fe9dcaa10352942902d511b37f5fb8dbd5
Maikon Henrique <maikonhenrique189@gmail.com>
2025-11-05 23:56:43 -0300

    feat(professor): adapta controller para usar DTO e ResponseEntity nas operações REST

--- src/main/java/com/fatec/avalia/controller/ProfessorController.java
+++ src/main/java/com/fatec/avalia/controller/ProfessorController.java
@@ -1,39 +1,51 @@
 package com.fatec.avalia.controller;
 
+import com.fatec.avalia.dto.ProfessorDTO;
 import com.fatec.avalia.entity.Professor;
 import com.fatec.avalia.service.ProfessorService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/professores")
 public class ProfessorController {
 
-    private final ProfessorService service;
+    private final ProfessorService professorService;
 
-    public ProfessorController(ProfessorService service) {
-        this.service = service;
+    public ProfessorController(ProfessorService professorService) {
+        this.professorService = professorService;
     }
 
     @GetMapping
-    public List<Professor> listar() {
-        return service.listarTodos();
+    public ResponseEntity<List<ProfessorDTO>> listarTodos() {
+        return ResponseEntity.ok(professorService.listarTodos());
     }
 
     @GetMapping("/{id}")
-    public Professor buscar(@PathVariable Long id) {
-        return service.buscarPorId(id);
+    public ResponseEntity<ProfessorDTO> buscarProfessorPorId (@PathVariable Long id) {
+        return ResponseEntity.ok(professorService.buscarPorId(id));
     }
 
     @PostMapping
-    public Professor criar(@RequestBody Professor professor) {
-        return service.salvar(professor);
+    public ResponseEntity<ProfessorDTO> salvar(@RequestBody ProfessorDTO professorDTO) {
+        return ResponseEntity.ok(professorService.salvar(professorDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProfessorDTO> atualizar(@PathVariable Long id, @RequestBody ProfessorDTO professorDTO) {
+        ProfessorDTO atualizado = professorService.atualizar(id, professorDTO);
+        return ResponseEntity.ok(atualizado);
     }
 
     @DeleteMapping("/{id}")
-    public void deletar(@PathVariable Long id) {
-        service.excluir(id);
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        professorService.excluir(id);
+        return ResponseEntity.noContent().build();
     }
+
+
 }
 
