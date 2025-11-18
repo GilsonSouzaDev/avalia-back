a8046dac6ce0e98b959466c8a73878bd83baef97
Maikon Henrique <maikonhenrique189@gmail.com>
2025-11-18 00:34:46 -0300

    refactor(pergunta): renomear campo conteudo para enunciado e ajustar relacionamentos Campo conteudo renomeado para enunciado para melhor clareza semântica Alterado relacionamento com Professor de professor para criadoPor Mudado o FetchType de EAGER → LAZY para melhorar desempenho e evitar carregamento desnecessário Removidos @JsonIgnoreProperties para evitar conflitos de serialização Criado método setAlternativasParaCadastro para garantir consistência da relação bidirecional com Alternativa Alterada a inicialização de alternativas para new ArrayList<>()

--- src/main/java/com/fatec/avalia/entity/Pergunta.java
+++ src/main/java/com/fatec/avalia/entity/Pergunta.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -19,20 +20,31 @@ public class Pergunta {
     private Long id;
 
     @Column(nullable = false, length = 500)
-    private String conteudo;
+    private String enunciado;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "professor_id")
-    @JsonIgnoreProperties({"disciplinas", "perguntas"})
-    private Professor professor;
+    //@JsonIgnoreProperties({"disciplinas", "perguntas"})
+    private Professor criadoPor;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "disciplina_id")
-    @JsonIgnoreProperties({"professores", "perguntas"})
+    //@JsonIgnoreProperties({"professores", "perguntas"})
     private Disciplina disciplina;
 
     @OneToMany(mappedBy = "pergunta", cascade = CascadeType.ALL, orphanRemoval = true)
-    @JsonIgnoreProperties("pergunta")
-    private List<Alternativa> alternativas;
+    //@JsonIgnoreProperties("pergunta")
+    private List<Alternativa> alternativas = new ArrayList<>();
 
+    public void setAlternativasParaCadastro(List<Alternativa> alternativas) {
+        // Remove as antigas se houver
+        this.alternativas.clear();
+        // Adiciona as novas e define a relação bidirecional
+        if (alternativas != null) {
+            for (Alternativa alt : alternativas) {
+                alt.setPergunta(this); // Seta o "pai" na "filha"
+                this.alternativas.add(alt);
+            }
+        }
+    }
 }
